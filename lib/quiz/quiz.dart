80e3a79a9856c47b91455586b34cc4c24c39afcd
trevor <treedprosser@gmail.com>
2022-09-29 09:54:41 -0600

    add project structure

---
+++ lib/quiz/quiz.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuizScreen extends StatelessWidget {
+  const QuizScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
