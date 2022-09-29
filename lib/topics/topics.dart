e2e024ce01d9c43e09d763b5c53be5072b0c09df
trevor <treedprosser@gmail.com>
2022-09-29 10:41:08 -0600

    add bottom nav bar

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutterquiz/shared/bottom_nav.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+    );
   }
 }
