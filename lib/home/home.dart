203f2ffcb36921971e91d3565a0a74f06fc3c0a2
trevor <treedprosser@gmail.com>
2022-09-30 10:15:06 -0600

    add user authentication

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutterquiz/login/login.dart';
+import 'package:flutterquiz/topics/topics.dart';
+import 'package:flutterquiz/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('about'),
-          onPressed: () => Navigator.pushNamed(context, '/about'),
-        ),
-      ),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('loading');
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('Error'),
+          );
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutterquiz/services/auth.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: ElevatedButton(
+        child: Text('Sign Out'),
+        onPressed: () async {
+          await AuthService().signOut();
+          Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+        }
+      ),
+    );
   }
 }
